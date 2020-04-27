ff1d987360542ba1b0dc04755f42473c61606e4c
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-27 17:02:29 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var prevScrollpos2 = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos2 = window.pageYOffset;
+  if (prevScrollpos2 > currentScrollPos2) {
+    document.getElementById("scroll-up").style.top = "0";
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("scroll-up").style.top = "34rem";
+    document.getElementById("navbar").style.top = "-200px";
+  }
+  prevScrollpos2 = currentScrollPos2;
+}
+
+
