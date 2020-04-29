93308e86f566da65c6481d6d07267820c2395209
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-30 01:52:30 +0200

    footer color

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,10 @@ var prevScrollpos2 = window.pageYOffset;
 window.onscroll = function() {
 var currentScrollPos2 = window.pageYOffset;
   if (prevScrollpos2 > currentScrollPos2) {
-    document.getElementById("scroll-up").style.top = "0";
+    document.getElementById("scroll-up").style.opacity = "100";
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("scroll-up").style.top = "34rem";
+    document.getElementById("scroll-up").style.opacity = "0";
     document.getElementById("navbar").style.top = "-100px";
   }
   prevScrollpos2 = currentScrollPos2;
